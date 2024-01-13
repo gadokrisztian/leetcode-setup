5d66c575bcd3dcdcd94df456117138bedfb09024
Gado Krisztian <85160134+gadokrisztian@users.noreply.github.com>
2024-01-13 08:14:36 +0100

    opt added

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
+#include <iostream>
 
 #include <fmt/ranges.h>
 #include <fmt/core.h>
 #include <vector>
 #include <fmt/chrono.h>
 
+#pragma GCC optimize("O3", "inline", "fast-math", "tree-vectorize")
+//#pragma GCC target("arch=native")
 
 using namespace std;
+
+// Boost i/o.
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
 
 int main() {
     int n = 54;
