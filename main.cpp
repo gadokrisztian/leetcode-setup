93796b9dec284c90408914c5eb25691a9825df2c
gadokrisztian <85160134+gadokrisztian@users.noreply.github.com>
2024-03-26 10:26:20 +0100

    checkpoint

--- main.cpp
+++ main.cpp
@@ -13,45 +13,79 @@ using namespace std;
 #pragma GCC optimize("O3", "inline", "fast-math", "tree-vectorize")
 // #pragma GCC target("arch=native")
 
-
 // Boost i/o.
-auto init = []() {
+auto init = []()
+{
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
   return 'c';
 }();
 
-class Solution {
+class Rectangle
+{
 public:
-  int largestRectangleArea(vector<int> &heights) {
-    if(heights.size() == 1) {
+  const int width;
+  const int height;
+  const int area;
+
+  Rectangle(int w, int h) : width(w), height(h), area(h * w) {}
+  Rectangle(int h) : width(1), height(h), area(h) {}
+
+  Rectangle operator||(const Rectangle &other) const
+  {
+    if (other.height > height)
+    {
+      if (other.area > area)
+      {
+        return other;
+      }
+
+      return Rectangle(width + other.width, height);
+    }
+    
+    if ((width + other.width) * other.height > area)
+    {
+      return Rectangle(width + other.width, other.height);
+    }
+    return Rectangle(width, height);
+  }
+};
+
+class Solution
+{
+public:
+  int largestRectangleArea(vector<int> &heights)
+  {
+    if (heights.size() == 1)
+    {
       return heights[0];
     }
-    pair<int, int> result = _aux(heights, 0);
-    return result.first * result.second;
+    return _aux(heights, 0).area;
   }
 
-  pair<int, int> _aux(vector<int> &heights, int i) {
-    if (heights.size() - i == 1) {
-      return {1, heights[0]};
-    } else {
-      pair<int, int> left = _aux(heights, i + 1);
-      if ((left.first + 1) * heights[i] >= left.first * left.second) {
-        return {left.first, heights[i]};
-      } else {
-        return left;
-      }
+  Rectangle _aux(vector<int> &heights, int i)
+  {
+    if (heights.size() - i-1 == 0)
+    {
+      return Rectangle(heights[0]);
+    }
+    else
+    {
+      Rectangle right = Rectangle(heights[i]);
+      Rectangle left = _aux(heights, i + 1);
+      return left || right;
     }
   }
 };
 
-int main() {
+int main()
+{
   Solution sol;
 
-  vector<int> heights = {2, 1, 5, 6, 2, 3};
+  // vector<int> heights = {2, 1, 5, 6, 2, 3};
   // vector<int> heights = {2, 4};
-  // vector<int> heights = {4, 2};
+  vector<int> heights = {4, 2};
 
   auto result = sol.largestRectangleArea(heights);
   fmt::println("{}", result);
